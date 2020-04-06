486e48afca57247f0525e8fd7978acbd794a5275
Aquila Sands <5774614+AquilaSands@users.noreply.github.com>
2020-04-06 23:12:25 +0100

    feat(guessing game): add random number and guess loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,15 +1,38 @@
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("What's your guess?");
+    let mut keep_going = true;
 
-    let mut guess = String::new();
+    let answer = thread_rng().gen_range(1, 101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line.");
+    while keep_going {
+        let mut guess = String::new();
 
-    println!("You guessed {}", guess)
+        println!("What's your guess?");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!(
+                    "You guessed {}, the answer was {}. You are correct.",
+                    guess, answer
+                );
+                keep_going = false
+            },
+        }
+    }
 }
